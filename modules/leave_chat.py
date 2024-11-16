0a598d2274be636a0a6f0bd590ec5a6ce7d0f5b6
Feanor-code <admin@sower.space>
2024-11-16 18:06:23 +0000

    format

--- modules/leave_chat.py
+++ modules/leave_chat.py
@@ -15,15 +15,15 @@ class LeaveChat:
             return True
         
     async def execute(self, session: Client) -> None:
-        me = await session.get_me()
 
         try:
+            me = await session.get_me()
             async for dialog in session.get_dialogs():
                 await session.leave_chat(dialog.chat.id, delete=True)
                 console.log(
                     "[*] User {name} has left the [yellow]{chat}[/] chat"
                     .format(name=me.first_name, chat=dialog.chat.first_name if dialog.chat.first_name else dialog.chat.title)
                 )
-                
+
         except Exception as error:
             console.print("Error : {}".format(error), style="bold white")
